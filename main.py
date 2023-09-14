f9245c6e0504338e6aed1aad4ceef5de2604394e
WEBobraz <56693534+WEBobraz@users.noreply.github.com>
2023-09-14 04:34:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+# 🚨 FOR Loop. Average Height
+
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+    student_heights[n] = int(student_heights[n])
