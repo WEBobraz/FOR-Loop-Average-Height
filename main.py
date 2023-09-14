95c7e4067c80ab5022bb83c89ae800faeb3b16ba
WEBobraz <56693534+WEBobraz@users.noreply.github.com>
2023-09-14 04:52:06 +0000

    replicate "sum" function

--- main.py
+++ main.py
@@ -9,9 +9,25 @@ for n in range(0, len(student_heights)):
     # [156, 178, 165, '171', '187']
     # [156, 178, 165, 171, '187']
     # [156, 178, 165, 171, 187]
+
 # simple way
-total_height = sum(student_heights)
-number_of_students = len(student_heights)
-average_height = round(total_height / number_of_students)
-print(average_height)
-#171 - result
+# total_height = sum(student_heights)
+# number_of_students = len(student_heights)
+# average_height = round(total_height / number_of_students)
+# print(average_height)
+# #171 - result
+
+# hard way
+# replicate "sum" function
+total_height = 0
+for hight in student_heights:
+    #"total_height = total_height + hight" the same "total_height += hight"
+    total_height += hight
+    # print(total_height)
+    # 156
+    # 334
+    # 499
+    # 670
+    # 857
+print(total_height)
+#857 - result
