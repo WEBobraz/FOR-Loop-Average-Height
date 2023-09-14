85610993e1118622d900860bb73c0833079bc544
WEBobraz <56693534+WEBobraz@users.noreply.github.com>
2023-09-14 04:47:53 +0000

    simple way

--- main.py
+++ main.py
@@ -3,9 +3,15 @@
 student_heights = input("Input a list of student heights ").split()
 for n in range(0, len(student_heights)):
     student_heights[n] = int(student_heights[n])
-    print(student_heights)
+    #print(student_heights)
     # [156, '178', '165', '171', '187']
     # [156, 178, '165', '171', '187']
     # [156, 178, 165, '171', '187']
     # [156, 178, 165, 171, '187']
     # [156, 178, 165, 171, 187]
+# simple way
+total_height = sum(student_heights)
+number_of_students = len(student_heights)
+average_height = round(total_height / number_of_students)
+print(average_height)
+#171 - result
