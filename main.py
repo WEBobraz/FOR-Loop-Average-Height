5cf2126038053ad3f44aca353fc9364d0f05fdcf
WEBobraz <56693534+WEBobraz@users.noreply.github.com>
2023-09-14 04:37:46 +0000

    Convert str to a list

--- main.py
+++ main.py
@@ -3,3 +3,9 @@
 student_heights = input("Input a list of student heights ").split()
 for n in range(0, len(student_heights)):
     student_heights[n] = int(student_heights[n])
+    print(student_heights)
+    # [156, '178', '165', '171', '187']
+    # [156, 178, '165', '171', '187']
+    # [156, 178, 165, '171', '187']
+    # [156, 178, 165, 171, '187']
+    # [156, 178, 165, 171, 187]
